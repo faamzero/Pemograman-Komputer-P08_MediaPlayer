caf17ac6495894fe4de62756ee22d811e58bd608
Fakhrul Anam <57746074+faamzero@users.noreply.github.com>
2020-05-08 13:56:35 +0700

    praktikum 9

---
+++ PHBMediaPlayer-master/src/phbmediaplayer/PHBMediaPlayer.java
@@ -0,0 +1,38 @@
+
+package phbmediaplayer;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author FaAmZero
+ */
+public class PHBMediaPlayer extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("PHP Media Player");
+        scene.setOnMouseClicked((MouseEvent event) -> {
+            if (event.getClickCount() == 2) {
+                stage.setFullScreen(true);
+            }
+        });
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
